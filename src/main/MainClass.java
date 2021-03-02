73175fd2a5c470ca7f6d410753e0265b9ec94605
younsik.choi <nadays3019@gmail.com>
2021-03-02 19:47:18 +0900

    프로제그 생성

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -1,5 +1,25 @@
 package main;
 
-public class MainClass {
+import java.util.Scanner;
 
+public class MainClass {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("main인 시작");
+		//kim - 로그인관련 기능 만드세요
+		//cho - 회원 관리 기능을 만드세요
+		
+		Scanner sc = new Scanner(System.in);
+		int num;
+		while(true) {
+			System.out.println("1.로그인 2.회원관리");
+			num = sc.nextInt();
+			switch(num) {
+			case 1  : break;
+			case 2 : break;
+			}
+		}
+	}
+	
 }
