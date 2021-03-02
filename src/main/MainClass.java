58ff35a4f419c3f043122a642153c8495d2c75f3
younsik.choi <nadays3019@gmail.com>
2021-03-02 19:59:03 +0900

    회원관리 기능 완성

--- src/main/MainClass.java
+++ src/main/MainClass.java
@@ -2,6 +2,8 @@ package main;
 
 import java.util.Scanner;
 
+import member.Member;
+
 public class MainClass {
 	
 	public static void main(String[] args) {
@@ -16,8 +18,12 @@ public class MainClass {
 			System.out.println("1.로그인 2.회원관리");
 			num = sc.nextInt();
 			switch(num) {
-			case 1  : break;
-			case 2 : break;
+			case 1  : 
+				break;
+			case 2 : 
+				Member m = new Member();
+				m.member();
+				break;
 			}
 		}
 	}
